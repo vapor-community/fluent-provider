5e5225d45ee3572f83a7d355184c6fae30c14eff
tanner0101 <me@tanner.xyz>
2017-03-20 18:25:12 +0100

    model + node convertible

--- Sources/VaporFluent/Model.swift
+++ Sources/VaporFluent/Model.swift
@@ -3,11 +3,27 @@ import Fluent
 import TypeSafeRouting
 import HTTP
 
-public protocol Model: Entity, JSONRepresentable, StringInitializable, ResponseRepresentable { }
+public protocol Model: Entity, JSONConvertible, NodeConvertible, StringInitializable, ResponseRepresentable { }
 
 extension Model {
     public func makeResponse() throws -> Response {
         return try makeJSON().makeResponse()
+    }
+    
+    public init(node: Node, in context: Context?) throws {
+        if context?.isRow == true {
+            try self.init(row: Row(node.wrapped))
+        } else {
+            try self.init(json: JSON(node.wrapped))
+        }
+    }
+    
+    public func makeNode(in context: Context?) throws -> Node {
+        if context?.isRow  == true {
+            return try Node(makeRow().wrapped)
+        } else {
+            return try Node(makeJSON().wrapped)
+        }
     }
 }
 
