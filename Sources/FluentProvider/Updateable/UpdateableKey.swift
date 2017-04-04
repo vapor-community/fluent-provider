17f4a4ce983be065b8c737326990ed48d80c2d0b
tanner0101 <me@tanner.xyz>
2017-04-04 13:53:47 +0200

    fix non public init

--- Sources/FluentProvider/Updateable/UpdateableKey.swift
+++ Sources/FluentProvider/Updateable/UpdateableKey.swift
@@ -1,11 +1,11 @@
 /// Represents an updateable key and
 /// the setter for when a new value is 
 /// given for the key.
-public class UpdateableKey<M> {
+public struct UpdateableKey<M> {
     public var name: String
     public var setter: (M, Node) throws -> ()
     
-    init<N: NodeInitializable>(
+    public init<N: NodeInitializable>(
         _ name: String,
         _ type: N.Type = N.self,
         _ setter: @escaping (M, N) throws -> ()
