86d2bf78899bcdde0a3c6159087c720e9137627e
tanner0101 <me@tanner.xyz>
2017-04-07 15:42:23 +0200

    migration improvements

--- Sources/FluentProvider/Prepare.swift
+++ Sources/FluentProvider/Prepare.swift
@@ -4,19 +4,20 @@ import Fluent
 /// Runs the droplet's `Preparation`s.
 public struct Prepare: Command {
     public let id: String = "prepare"
-
+    
     public let signature: [Argument] = [
-        Option(name: "revert"),
+        Option(name: "revert", help: ["Reverts preparations"]),
+        Option(name: "all", help: ["Use with `--revert` to revert entire database"])
     ]
-
+    
     public let help: [String] = [
         "runs the droplet's preparations"
     ]
-
+    
     public let console: ConsoleProtocol
     public let preparations: [Preparation.Type]
     public let database: Database?
-
+    
     public init(
         console: ConsoleProtocol,
         preparations: [Preparation.Type],
@@ -26,67 +27,46 @@ public struct Prepare: Command {
         self.preparations = preparations
         self.database = database
     }
-
+    
     public func run(arguments: [String]) throws {
         guard preparations.count > 0 else {
             return
         }
-
+        
         guard let database = database else {
             throw CommandError.general("Can not run preparations, droplet has no database")
         }
-
+        
         if arguments.option("revert")?.bool == true {
-            try revert(database)
-        } else {
-            try prepare(database)
-        }
-    }
-
-    private func prepare(_ database: Database) throws {
-        try preparations.forEach { preparation in
-            let name = "\(preparation.self)"
-
-            let hasPrepared = try database.hasPrepared(preparation)
-            // only prepare the unprepared
-            guard !hasPrepared else { 
-                if let model = preparation as? Entity.Type {
-                    // the model has already prepared, so we must exit early
-                    // to prevent a duplicate preparation.
-                    // the model.database property must be set manually
-                    model.database = database
+            if arguments.option("all")?.bool == true {
+                guard console.confirm("Are you sure you want to revert the entire database?", style: .warning) else {
+                    console.error("Reversion cancelled")
+                    return
                 }
-                return 
+                
+                try database.revertAll(preparations)
+                console.print("Removing metadata")
+                try database.revertMetadata()
+                console.success("Reversion complete")
+            } else {
+                let (batch, toBeReverted) = try database.previewRevertBatch(preparations)
+                let list = toBeReverted.map { "\($0)" }.joined(separator: ", ")
+                
+                guard toBeReverted.count > 0 else {
+                    console.print("Nothing to be reverted")
+                    return
+                }
+                
+                guard console.confirm("Are you sure you want to revert batch \(batch) (\(list))?", style: .warning) else {
+                    console.error("Reversion cancelled")
+                    return
+                }
+                
+                try database.revertBatch(preparations)
             }
-
-            console.info("Preparing \(name)")
-            try database.prepare(preparation)
-            console.success("Prepared \(name)")
+        } else {
+            try database.prepare(preparations)
+            console.info("Database prepared")
         }
-
-        console.info("Database prepared")
-    }
-
-    private func revert(_ database: Database) throws {
-        guard console.confirm("Are you sure you want to revert the database?", style: .warning) else {
-            console.error("Reversion cancelled")
-            return
-        }
-
-
-        try preparations.reversed().forEach { preparation in
-            let name = "\(preparation.self)"
-
-            let hasPrepared = try database.hasPrepared(preparation)
-            // if not prepared yet, no need to revert
-            guard hasPrepared else { return }
-
-            try preparation.revert(database)
-            console.success("Reverted \(name)")
-        }
-
-        console.print("Removing metadata")
-        try database.removeMetadata()
-        console.success("Reversion complete")
     }
 }
