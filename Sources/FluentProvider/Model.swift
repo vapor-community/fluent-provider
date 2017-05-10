9bdd141b5507644ce3bb0fd5a057aecdfe9678ea
tanner0101 <me@tanner.xyz>
2017-05-10 11:47:20 +0100

    update to parameterizable

--- Sources/FluentProvider/Model.swift
+++ Sources/FluentProvider/Model.swift
@@ -2,21 +2,29 @@ import JSON
 import Fluent
 import HTTP
 
-public protocol Model: Entity, StringInitializable { }
+public protocol Model: Entity, Parameterizable { }
+
+// MARK: Parameterizable
+extension StringInitializable where Self: Entity {
+    /// the unique key to use as a slug in route building
+    static var uniqueSlug: String {
+        return foreignIdKey
+    }
+    
+    // returns the found model for the resolved url parameter
+    static func make(for parameter: String) throws -> Self {
+        let id = Identifier(parameter)
+        guard let found = try find(id) else {
+            throw Abort(.notFound, reason: "No \(Self.self) with that identifier was found.")
+        }
+        return found
+    }
+}
+
+// MARK: JSON
 
 extension ResponseRepresentable where Self: JSONRepresentable {
     public func makeResponse() throws -> Response {
         return try makeJSON().makeResponse()
-    }
-}
-
-// MARK: StringInitializable
-extension StringInitializable where Self: Entity {
-    public init?(_ string: String) throws {
-        if let model = try Self.find(string) {
-            self = model
-        } else {
-            return nil
-        }
     }
 }
